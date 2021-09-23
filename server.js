dc9e6fa406ea33bdbbc40d1a733631a39187d343
Max Manayan <maximillian521@gmail.com>
2021-09-23 10:36:22 -0600

    Heroku PORT added to server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const resolvers = require("./graphQL/resolvers");
 
 // constants
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -72,6 +72,6 @@ app.use((err, req, res, next) => {
 });
 
 // exports
-module.exports = app.listen(process.env.PORT || PORT, () => {
+module.exports = app.listen(PORT, () => {
   console.log(`Climate-app listening on http://localhost:${PORT}`);
 });
